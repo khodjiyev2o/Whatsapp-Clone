5eb2053f6ba3fb6f8749590eb6dd35128d5f7bb9
khodjiyev2o <92217667+khodjiyev2o@users.noreply.github.com>
2022-06-10 14:48:50 +0900

    beginning

--- chat/asgi.py
+++ chat/asgi.py
@@ -19,3 +19,6 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     # Just HTTP for now. (We can add other protocols later.)
 })
+
+
+
--- lobby/views.py
+++ lobby/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'lobby/index.html', {})
+
+
+def room(request, room_name):
+    return render(request, 'chat/room.html', {
+        'room_name': room_name
+    })
