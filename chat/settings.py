7c96ee33fd202d0ece05eb3b990bc326e7f21844
khodjiyev2o <92217667+khodjiyev2o@users.noreply.github.com>
2022-06-13 08:08:23 +0900

    fianlly

--- chat/settings.py
+++ chat/settings.py
@@ -55,11 +55,8 @@ ROOT_URLCONF = 'chat.urls'
 ASGI_APPLICATION = "chat.asgi.application"
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
+    }
 }
 
 
