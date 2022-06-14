075e597becdc199acc63154cfae4a17efc201db4
khodjiyev2o <92217667+khodjiyev2o@users.noreply.github.com>
2022-06-14 19:55:18 +0900

    consumers.py

--- lobby/consumers.py
+++ lobby/consumers.py
@@ -4,4 +4,23 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
-    print('hello world!')
+    async def connect(self):
+
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        # Join room group
+        await self.channel_layer.group_add(
+                self.room_group_name,
+                self.channel_name,
+                print(self.channel_name)
+
+        )
+
+    async def disconnect(self,close_code):
+
+        # Join room group
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
