cfb31854046622f6c6a1e44d2524e52dd0d6a80a
khodjiyev2o <92217667+khodjiyev2o@users.noreply.github.com>
2022-06-20 14:04:49 +0900

    models

--- lobby/admin.py
+++ lobby/admin.py
@@ -1,5 +1,36 @@
 from django.contrib import admin
 from .models import Message,Thread
+from django import forms
+from django.core.exceptions import ValidationError
+from django.db.models import Q
 # Register your models here.
+
+
+class ChatMessage(admin.TabularInline):
+    model = Message
+
+
+class ThreadForm(forms.ModelForm):
+    def clean(self):
+
+            super(ThreadForm, self).clean()
+            first_person = self.cleaned_data.get('first_person')
+            second_person = self.cleaned_data.get('second_person')
+            lookup1 = Q(first_person=first_person) & Q(second_person=second_person)
+            lookup2 = Q(first_person=second_person) & Q(second_person=first_person)
+            lookup = Q(lookup1 | lookup2)
+            qs = Thread.objects.filter(lookup)
+            if qs.exists():
+                raise ValidationError(f'Thread between {first_person} and {second_person} already exists.')
+
+
+
+class ThreadAdmin(admin.ModelAdmin):
+    inlines = [ChatMessage]
+
+    class Meta:
+        model = Thread
+
+
+admin.site.register(Thread, ThreadAdmin)
 admin.site.register(Message)
-admin.site.register(Thread)
