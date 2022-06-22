97f9ada3b965521eb1ae1802eeeae539268e966a
khodjiyev2o <92217667+khodjiyev2o@users.noreply.github.com>
2022-06-22 18:40:02 +0900

    thread_test

--- lobby/tests.py
+++ lobby/tests.py
@@ -10,20 +10,36 @@ class MessageTestCase(TestCase):
 
     def setUp(self):
 
-        user = User.objects.create(username="Samandar",email="samandar@gmail.com",password="sam12345")
-        user1 = User.objects.create(username="Anvarov", email="samandar@gmail.com", password="sam12345")
-        thread = Thread.objects.create(first_person=user,second_person=user1,)
+        user1 = User.objects.create(username="Samandar",email="samandar@gmail.com",password="sam12345")
+        user2 = User.objects.create(username="Anvarov", email="samandar@gmail.com", password="sam12345")
+        user3 = User.objects.create(username="Abror", email="samandar@gmail.com", password="sam12345")
 
-        Message.objects.create(context="hello",owner=user,thread=thread)
+        #user4 = User.objects.create(username="Kamron", email="samandar@gmail.com", password="sam12345")
+
+
+        thread1 = Thread.objects.create(first_person=user1,second_person=user2)
+        thread2 = Thread.objects.create(first_person=user1, second_person=user3)
+
+        #thread3 = Thread.objects.create(first_person=user1, second_person=user4)
+        #thread4 = Thread.objects.create(first_person=user2, second_person=user3)
+
+        Message.objects.create(context="hello",owner=user2,thread=thread1)
+        Message.objects.create(context="uzb", owner=user1, thread=thread1)
+
+
+
+
+
 
 
     def test_thread_counter(self):
         thread = Thread.objects.count()
-        self.assertEqual(thread, 1)
+        self.assertEqual(thread, 2)
 
     def test_message_counter(self):
         message = Message.objects.count()
-        self.assertEqual(message,1)
+
+        self.assertEqual(message,2)
 
     def test_message_context(self):
         user1 = User.objects.get(username="Samandar")
@@ -31,3 +47,17 @@ class MessageTestCase(TestCase):
         message = Message.objects.get(owner=user1)
         for word in prohibited_words:
             self.assertNotEqual(message.context, word)
+
+    def test_thread_unique(self):
+        samandar= User.objects.get(username="Samandar")
+        anvarov = User.objects.get(username="Anvarov")
+        thread = Thread.objects.get(first_person=samandar,second_person=anvarov)
+        messages = Message.objects.filter(thread=thread)
+
+        def thread_test(messages,user1,user2):
+            for message in messages:
+                if message.owner != user1 and message.owner != user2:
+                    return False
+            return True
+
+        self.assertTrue(thread_test(messages,samandar,anvarov))
